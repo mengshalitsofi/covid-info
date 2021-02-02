d572a7dac2419e341c1c5fbf138c4e9b3fea6527
mengshalitsofi <mengshalitsofi@gmail.com>
2021-02-02 11:58:27 -0800

    setting up the environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 require 'pry'
-require 'httparty'
+#require 'httparty'
 
 require 'bundler'
 Bundler.require
 
 require_all 'lib'
 #require_relative('../lib/api')
+#require_relative('../lib/covid_controller')
+#require_relative('../lib/covid_location')
--- lib/covid_controller.rb
+++ lib/covid_controller.rb
@@ -1,7 +1,11 @@
 class CovidController
     def call
         puts "Welcome to the COVID-19 information center!"
+        self.prompt_for_state
+    end
+
+    def prompt_for_state
         puts "\nTo get started, please enter your state (e.g. WA):"
-        puts ""
+        input = gets.strip
     end
 end
