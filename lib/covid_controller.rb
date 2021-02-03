e5f53f186cbb9276e3ec7bbb4d80451333ff6c62
mengshalitsofi <mengshalitsofi@gmail.com>
2021-02-02 17:56:34 -0800

    adding colors

--- lib/covid_controller.rb
+++ lib/covid_controller.rb
@@ -5,7 +5,7 @@ class CovidController
     end
 
     def prompt_for_state
-        puts "\nTo get started, please enter your state (e.g. WA). Enter 'exit' to quit."
+        puts "Please enter your state (e.g. WA). Enter 'exit' to quit."
         input = ""
         loop do
             input = gets.strip.upcase            
@@ -25,15 +25,15 @@ class CovidController
     def state_covid_options(state_name)
         state = State.get_or_create_state_info(state_name)
 
-        puts "\nViewing information for #{state.name} (last updated on #{state.last_update_date}):"
+        puts "Viewing information for #{state.name} (last updated on #{state.last_update_date}):"
         puts "----------------------------------------------------------------"
         puts "Please choose from the following list of options:"
-        puts "1. View total positive cases"
-        puts "2. View number of current hospitalized people"
-        puts "3. View last day's positive cases"
-        puts "4. View total death count"
-        puts "5. View last day's death count"
-        puts "6. Back to main menu\n"
+        puts "1. View total positive cases".green
+        puts "2. View number of current hospitalized people".green
+        puts "3. View last day's positive cases".green
+        puts "4. View total death count".green
+        puts "5. View last day's death count".green
+        puts "6. Back to main menu\n".green
         
         self.user_selection(state, state_name)
     end
@@ -41,21 +41,26 @@ class CovidController
     def user_selection(state, state_name)
         input = gets.strip
         if input == "1"
-            puts "Total positive cases in #{state.name} is #{state.total_positive_cases}"
+            puts "Total positive cases in #{state.name} is #{state.total_positive_cases}".blue
+            self.state_covid_options(state_name)
         elsif input == "2"
-            puts "Number of current hospitalized people in #{state.name} is #{state.in_hospital}"
+            puts "Number of current hospitalized people in #{state.name} is #{state.in_hospital}".blue
+            self.state_covid_options(state_name)
         elsif input == "3"
-            puts "Last day's positive cases in #{state.name} is #{state.last_day_positive}"
+            puts "Last day's positive cases in #{state.name} is #{state.last_day_positive}".blue
+            self.state_covid_options(state_name)
         elsif input == "4"
-            puts "Total death count in #{state.name} is #{state.total_deaths}"
+            puts "Total death count in #{state.name} is #{state.total_deaths}".blue
+            self.state_covid_options(state_name)
         elsif input == "5"
-            puts "Last day's death count in #{state.name} is #{state.last_day_deaths}"
+            puts "Last day's death count in #{state.name} is #{state.last_day_deaths}".blue
+            self.state_covid_options(state_name)
         elsif input == "6"
             self.prompt_for_state
         else
-            puts "Invalid selection.\n"
+            puts "Invalid selection.\n".red
             self.state_covid_options(state_name)
-        end
+        end        
         
     end
 
